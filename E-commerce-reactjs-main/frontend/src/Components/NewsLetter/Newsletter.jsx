6c3f7e2e040beecd20a7655c05cdf7a27df9d2d7
piyush <piyushkv49@gmail.com>
2026-02-02 18:24:24 +0530

    uui changes

--- E-commerce-reactjs-main/frontend/src/Components/NewsLetter/Newsletter.jsx
+++ E-commerce-reactjs-main/frontend/src/Components/NewsLetter/Newsletter.jsx
@@ -7,7 +7,7 @@ const Newsletter = () => {
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
             <input type='email' placeholder='Your Email id'/>
-            <button>Subscribe</button>
+            <button>Subscribe channel</button>
         </div>
     </div>
   )
